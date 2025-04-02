dfc46ffd6c74c66bc446370f5328580eb4621f3f
Passakorn-Boonyong <s6604062663205@email.kmutnb.ac.th>
2025-04-02 09:26:06 +0700

    Aroiwai

---
+++ Aroiwai/frontend/src/assets/assets.js
@@ -0,0 +1,341 @@
+import basket_icon from './basket_icon.png'
+import logo from './logo.png'
+import header_img from './header_img.png'
+import search_icon from './search_icon.png'
+import menu_1 from './menu_1.png'
+import menu_2 from './menu_2.png'
+import menu_3 from './menu_3.png'
+import menu_4 from './menu_4.png'
+import menu_5 from './menu_5.png'
+import menu_6 from './menu_6.png'
+import menu_7 from './menu_7.png'
+import menu_8 from './menu_8.png'
+import rating_starts from './rating_starts.png'
+import food_1 from './food_1.png'
+import food_2 from './food_2.png'
+import food_3 from './food_3.png'
+import food_4 from './food_4.png'
+import food_5 from './food_5.png'
+import food_6 from './food_6.png'
+import food_7 from './food_7.png'
+import food_8 from './food_8.png'
+import food_9 from './food_9.png'
+import food_10 from './food_10.png'
+import food_11 from './food_11.png'
+import food_12 from './food_12.png'
+import food_13 from './food_13.png'
+import food_14 from './food_14.png'
+import food_15 from './food_15.png'
+import food_16 from './food_16.png'
+import food_17 from './food_17.png'
+import food_18 from './food_18.png'
+import food_19 from './food_19.png'
+import food_20 from './food_20.png'
+import food_21 from './food_21.png'
+import food_22 from './food_22.png'
+import food_23 from './food_23.png'
+import food_24 from './food_24.png'
+import food_25 from './food_25.png'
+import food_26 from './food_26.png'
+import food_27 from './food_27.png'
+import food_28 from './food_28.png'
+import food_29 from './food_29.png'
+import food_30 from './food_30.png'
+import food_31 from './food_31.png'
+import food_32 from './food_32.png'
+
+import add_icon_white from './add_icon_white.png'
+import add_icon_green from './add_icon_green.png'
+import remove_icon_red from './remove_icon_red.png'
+import app_store from './app_store.png'
+import play_store from './play_store.png'
+import linkedin_icon from './linkedin_icon.png'
+import facebook_icon from './facebook_icon.png'
+import twitter_icon from './twitter_icon.png'
+import cross_icon from './cross_icon.png'
+import selector_icon from './selector_icon.png'
+
+export const assets = {
+    logo,
+    basket_icon,
+    header_img,
+    search_icon,
+    rating_starts,
+    add_icon_green,
+    add_icon_white,
+    remove_icon_red,
+    app_store,
+    play_store,
+    linkedin_icon,
+    facebook_icon,
+    twitter_icon,
+    cross_icon,
+    selector_icon
+}
+
+export const menu_list = [
+    {
+        menu_name: "Thai Food",
+        menu_image: menu_1
+    },
+    {
+        menu_name: "Korean Food",
+        menu_image: menu_2
+    },
+    {
+        menu_name: "Japanese Food",
+        menu_image: menu_3
+    },
+    {
+        menu_name: "Chinese Food",
+        menu_image: menu_4
+    },
+    {
+        menu_name: "Western Food",
+        menu_image: menu_5
+    },
+    {
+        menu_name: "Indian Food",
+        menu_image: menu_6
+    },
+    {
+        menu_name: "Noodles",
+        menu_image: menu_7
+    },
+    {
+        menu_name: "Desserts",
+        menu_image: menu_8
+    }]
+
+export const food_list = [
+    {
+        food_id: 1,
+        food_name: "Pad Thai",
+        food_image: food_1,
+        food_price: 12,
+        food_desc: "Food providesA Thai noodle stir fry with a sweet-savoury-sour sauce scattered with crushed peanuts. essential nutrients for overall health and well-being",
+        food_category: "Thai Food"
+    },
+    {
+        food_id: 2,
+        food_name: "Pad Kraprao",
+        food_image: food_2,
+        food_price: 18,
+        food_desc: "A real Thai classic Pad Krapao topped with, or served over, a crispy fried egg with a slightly runny yolk.",
+        food_category: "Thai Food"
+    }, {
+        food_id: 3,
+        food_name: "Kao Soi",
+        food_image: food_3,
+        food_price: 16,
+        food_desc: "A deliciously rich, creamy, slightly spicy yellow curry dish originating in Northern Thailand.",
+        food_category: "Thai Food"
+    }, {
+        food_id: 4,
+        food_name: "Thai Curry Chicken",
+        food_image: food_4,
+        food_price: 24,
+        food_desc: "Flavorful and aromatic, typically made with a base of coconut milk, curry paste, and a variety of herbs and spices.",
+        food_category: "Thai Food"
+    }, {
+        food_id: 5,
+        food_name: "Bibimbap",
+        food_image: food_5,
+        food_price: 14,
+        food_desc: "A bowl of warm white rice topped with namul (sautéed or blanched seasoned vegetables) and gochujang (chili pepper paste).",
+        food_category: "Korean Food"
+    }, {
+        food_id: 6,
+        food_name: "Kimchi",
+        food_image: food_6,
+        food_price: 12,
+        food_desc: "A traditional Korean fermented vegetable made from Chinese cabbage, radish, green onion, red pepper powder, garlic, ginger, and fermented shrimp",
+        food_category: "Korean Food"
+    }, {
+        food_id: 7,
+        food_name: "Jajangmyeon",
+        food_image: food_7,
+        food_price: 20,
+        food_desc: "Korean Chinese noodle dish topped with a thick sauce made of chunjang, diced pork, and vegetables.",
+        food_category: "Korean Food"
+    }, {
+        food_id: 8,
+        food_name: "Tokpokki",
+        food_image: food_8,
+        food_price: 15,
+        food_desc: "Korean traditional food which has sticks of rounded rice cake as the main ingredient.",
+        food_category: "Korean Food"
+    }, {
+        food_id: 9,
+        food_name: "Sushi",
+        food_image: food_9,
+        food_price: 14,
+        food_desc: "A staple rice dish of Japanese cuisine, consisting of cooked rice flavoured with vinegar and vegetable, egg, or raw seafood garnishes and served cold.",
+        food_category: "Japanese Food"
+    }, {
+        food_id: 10,
+        food_name: "Tonkatsu",
+        food_image: food_10,
+        food_price: 22,
+        food_desc: "Japanese dish that consists of a breaded, deep-fried pork cutlet.",
+        food_category: "Japanese Food"
+    }, {
+        food_id: 11,
+        food_name: "Japanese Curry",
+        food_image: food_11,
+        food_price: 10,
+        food_desc: "The sauce resembles a rich, thick demi-glace layered with spices and earthy flavors.",
+        food_category: "Japanese Food"
+    }, {
+        food_id: 12,
+        food_name: "Takoyaki",
+        food_image: food_12,
+        food_price: 12,
+        food_desc: "A ball-shaped Japanese snack made of a wheat flour-based batter and cooked in a special molded pan.",
+        food_category: "Japanese Food"
+    },
+    {
+        food_id: 13,
+        food_name: "Fried Rice",
+        food_image: food_13,
+        food_price: 12,
+        food_desc: "Cooked rice, beaten eggs, spring onions or scallions, oil and soy sauce cooked on high heat in a wok.",
+        food_category: "Chinese Food"
+    },
+    {
+        food_id: 14,
+        food_name: "Wonton Soup",
+        food_image: food_14,
+        food_price: 18,
+        food_desc: "A simple, light, Chinese classic with juicy pork and shrimp dumplings in a flavorsome broth.",
+        food_category: "Chinese Food"
+    }, {
+        food_id: 15,
+        food_name: "Mala Stew",
+        food_image: food_15,
+        food_price: 16,
+        food_desc: "A soup or stew menu that uses various ingredients that you like or want to eat, such as meat or vegetables.",
+        food_category: "Chinese Food"
+    }, {
+        food_id: 16,
+        food_name: "Dim sum",
+        food_image: food_16,
+        food_price: 24,
+        food_desc: "A traditional Chinese meal made up of small plates of dumplings.",
+        food_category: "Chinese Food"
+    }, {
+        food_id: 17,
+        food_name: "Pizza",
+        food_image: food_17,
+        food_price: 14,
+        food_desc: "A dish made typically of flattened bread dough spread with a savory mixture.",
+        food_category: "Western Food"
+    }, {
+        food_id: 18,
+        food_name: "Burger",
+        food_image: food_18,
+        food_price: 12,
+        food_desc: "A patty of ground beef grilled and placed between two halves of a bun.",
+        food_category: "Western Food"
+    }, {
+        food_id: 19,
+        food_name: "Tacos",
+        food_image: food_19,
+        food_price: 20,
+        food_desc: "A crispy or soft corn or wheat tortilla that is folded or rolled and stuffed with a mixture.",
+        food_category: "Western Food"
+    }, {
+        food_id: 20,
+        food_name: "French Fries",
+        food_image: food_20,
+        food_price: 15,
+        food_desc: "Deep fried potatoes sometimes thin-cut strips.",
+        food_category: "Western Food"
+    }, {
+        food_id: 21,
+        food_name: "Briyani",
+        food_image: food_21,
+        food_price: 14,
+        food_desc: "A celebratory rice and meat dish cherished in the Indian sub-continent.",
+        food_category: "Indian Food"
+    }, {
+        food_id: 22,
+        food_name: "Samosa",
+        food_image: food_22,
+        food_price: 22,
+        food_desc: "A pastry with a savory filling, mostly vegetables, spiced potatoes, onions, and peas.",
+        food_category: "Indian Food"
+    }, {
+        food_id: 23,
+        food_name: "Pakora",
+        food_image: food_23,
+        food_price: 10,
+        food_desc: "An Indian dish consisting of pieces of vegetable, chicken, etc, dipped in a spiced batter and deep-fried.",
+        food_category: "Indian Food"
+    }, {
+        food_id: 24,
+        food_name: "Chapati",
+        food_image: food_24,
+        food_price: 12,
+        food_desc: "Made of whole-wheat flour known as atta, mixed into dough with water, oil, and salt in a mixing utensil",
+        food_category: "Indian Food"
+    },
+    {
+        food_id: 25,
+        food_name: "Carbonara Pasta",
+        food_image: food_25,
+        food_price: 12,
+        food_desc: "A pasta dish made with fatty cured pork, hard cheese, eggs, salt, and black pepper.",
+        food_category: "Noodles"
+    },
+    {
+        food_id: 26,
+        food_name: "Yakisoba",
+        food_image: food_26,
+        food_price: 18,
+        food_desc: "A popular noodle stir-fried dish in Japan.",
+        food_category: "Noodles"
+    }, {
+        food_id: 27,
+        food_name: "Spaghetti",
+        food_image: food_27,
+        food_price: 16,
+        food_desc: "A long, thin, solid, cylindrical pasta serves with slow-simmered sauce that is often made with tomatoes, garlic, and herbs.",
+        food_category: "Noodles"
+    }, {
+        food_id: 28,
+        food_name: "Pho",
+        food_image: food_28,
+        food_price: 24,
+        food_desc: "A Vietnamese soup consisting of bone broth, rice noodles, and thinly sliced meat.",
+        food_category: "Noodles"
+    }, {
+        food_id: 29,
+        food_name: "Tiramisu Cake",
+        food_image: food_29,
+        food_price: 14,
+        food_desc: "Made of moist and fluffy vanilla layers soaked with a mixture of brandy, coffee liqueur and espresso.",
+        food_category: "Desserts"
+    }, {
+        food_id: 30,
+        food_name: "Apple Pie",
+        food_image: food_30,
+        food_price: 12,
+        food_desc: "A pie in which the principal filling is apples.",
+        food_category: "Desserts"
+    }, {
+        food_id: 31,
+        food_name: "Banana Pudding",
+        food_image: food_31,
+        food_price: 20,
+        food_desc: "Made with pudding mix and condensed milk. Layer it with vanilla wafers and banana slices.",
+        food_category: "Desserts"
+    }, {
+        food_id: 32,
+        food_name: "Cheese Cake",
+        food_image: food_32,
+        food_price: 15,
+        food_desc: "A dessert made with a soft fresh cheese, eggs, and sugar.",
+        food_category: "Desserts"
+    }
+]
---
+++ Aroiwai/frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className='header-contents'>
+                <h2>Order your food here</h2>
+                <p>Aroiwai, a website where you can order a variety of food without leaving home.</p>
+                <button>View Menu</button>
+            </div>
+        </div>
+    )
+}
+
+export default Header
